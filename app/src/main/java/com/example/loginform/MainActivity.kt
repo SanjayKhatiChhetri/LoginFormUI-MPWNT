826fe4437bc88c6761a1e05f884d623610bdea6b
SanjayKhatiChhetri <SanjayKhati579@gmail.com>
2024-02-25 05:21:22 +0200

    "Implemented LoginForm with Jetpack Compose and Material Design"

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.loginform
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.loginform.ui.theme.LoginFormTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginForm()
                 }
             }
         }
@@ -30,17 +49,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginForm() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisiblity by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoginFormTheme {
-        Greeting("Android")
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp)
+    ) {
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it},
+            label = { Text(text = "Username")},
+            singleLine = true,
+            trailingIcon = { Icon(Icons.Filled.Email, contentDescription = null)},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = "Password")},
+            singleLine = true,
+            visualTransformation = if (passwordVisiblity) VisualTransformation.None
+            else PasswordVisualTransformation(),
+            trailingIcon = { Icon(Icons.Filled.Lock, contentDescription = null)},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+
+        FilledIconButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+            ) {
+            Text(text = "Submit")
+        }
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LoginFormPreview() {
+    LoginFormTheme {
+        LoginForm()
+    }
+}
+
+
